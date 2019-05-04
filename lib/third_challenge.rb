889a88daee84e3cecad3045cf71f2df027483732
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-04 20:42:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,11 +3,9 @@ def third_challenge
    :montague => {
       :patriarch => { name:"Lord Montague", age:'53'
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {name:"Lady Montague", age:"54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {name:"Romeo",age:"15",status:"alive"
         },
       :hero_friends => []
    }, 
