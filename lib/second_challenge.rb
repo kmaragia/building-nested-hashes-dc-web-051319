129271802c837c06715fdc356bf018719af8f8c4
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-04 20:37:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {patriarch:{},matriarch:{},hero:{},hero_friends:[]
       }, 
-    :capulet => {
+    :capulet => {patriarch:{},matriarch:{},heroine:{},heroine_friends:[]
       #your key/value pairs here!
       }}
 
